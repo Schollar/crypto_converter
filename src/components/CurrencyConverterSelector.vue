3e93ae0f1a91c06ac2b486139d37f8380dc69130
Schollar <cschollar22@gmail.com>
2022-05-17 17:09:51 -0600

    Started working on the currency conversion component

--- src/components/CurrencyConverterSelector.vue
+++ src/components/CurrencyConverterSelector.vue
@@ -2,9 +2,7 @@
   <div>
     <!-- TO DO: use https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies.json to grab list exchange rate for and make a list of maybe top 10 currencies to convert to -->
     <v-select
-      return-object
-      :items="crypto_data"
-      item-text="name"
+      :items="currencies"
       label="Standard"
       v-model="selected"
       class="select"
@@ -15,6 +13,37 @@
 <script>
 export default {
   name: "currency-converter-selector",
+  data() {
+    return {
+      currency_data: undefined,
+      currencies: ["CAD", "EUR", "GPB"],
+    };
+  },
+  mounted() {
+    this.get_currency_data();
+  },
+  methods: {
+    get_currency_data() {
+      // Grabbing The prices of other currencies using USD as the base
+      this.$axios
+        .request({
+          url: `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd.json`,
+          method: "GET",
+        })
+        // Setting character tasks list to data sent back
+        .then((response) => {
+          response.forEach();
+          this.currency_data = response.data.usd;
+        })
+        .catch((error) => {
+          error;
+          this.$root.$emit(
+            "error_message",
+            "Something went wrong getting characters tasks"
+          );
+        });
+    },
+  },
 };
 </script>
 
