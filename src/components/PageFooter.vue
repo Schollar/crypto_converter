2f19ff932b70ddfce5b753b1c2c49ac584b719a5
Schollar <cschollar22@gmail.com>
2022-05-29 00:55:31 -0600

    Changed up Page heading, along with styling the footer and beginning mobile first design patterns

--- src/components/PageFooter.vue
+++ src/components/PageFooter.vue
@@ -2,10 +2,21 @@
   <div class="footer_container">
     <section class="info_container">
       <p>
-        *The API service used for currency data is located <span>here</span>,
-        and the currency data is only updated once a day.<br />
+        The currency data is only updated once a day so it may not reflect the
+        same prices as a real time service provides.<br />
+        *The API service used for currency data is located
+        <span
+          ><a
+            href="https://github.com/fawazahmed0/currency-api#readme"
+            target="__blank"
+            >here</a
+          > </span
+        ><br />
         If you would like to donate so I am able to upgrade to a live service
-        you can do so <span>here</span>
+        you can do so
+        <span
+          ><a href="https://paypal.me/schollar4" target="__blank">here</a></span
+        >
       </p>
     </section>
   </div>
@@ -17,8 +28,18 @@ export default {
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 .footer_container {
+  margin-top: 30%;
   color: #598392;
+}
+.info_container {
+  display: grid;
+  place-items: center;
+
+  > p {
+    width: 80%;
+    font-size: 0.8em;
+  }
 }
 </style>
--- src/components/PageHeader.vue
+++ src/components/PageHeader.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="heading_container">
     <h1>Coin Price</h1>
-    <p>Check prices of popular crypto in your local currency.</p>
+    <p>Check prices of different Crypto coins in different currencies.</p>
   </div>
 </template>
 
@@ -13,7 +13,18 @@ export default {
 
 <style lang="scss" scoped>
 .heading_container {
-  margin-top: 15%;
+  margin-top: 5%;
+  display: grid;
+  place-items: center;
+  width: 100%;
+  text-align: center;
   color: #598392;
+  font-family: "Lato", sans-serif;
+  > h1 {
+    font-size: 3rem;
+  }
+  > p {
+    max-width: 80%;
+  }
 }
 </style>
